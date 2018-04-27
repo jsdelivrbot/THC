a731668ff5807460dcf4252844e3dce28e7ab910
Chillout Susan <ChilloutSusan@users.noreply.github.com>
2018-04-27 23:10:09 +0200

    Create THC.php

---
+++ THC.php
@@ -0,0 +1,10 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+?>
+{
+    "css": "https://rawgit.com/ChilloutSusan/THC/master/thc.css",
+    "social": {
+        "icon": "https://i.imgur.com/B7iH1PS.png",
+        "link": "http://discord.gg/jXXwQeB"
+    }
+}
